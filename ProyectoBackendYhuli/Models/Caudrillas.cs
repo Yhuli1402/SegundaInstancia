1ef8e010470e5cec5483bf6cb7aadfe8910aafae
Yhuli1402 <yhuli.fulguera@uab.edu.bo>
2023-12-08 10:11:19 -0400

    subiendo el modelado

---
+++ ProyectoBackendYhuli/Models/Caudrillas.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProyectoBackendYhuli.Models
+{
+    public class Cuadrillas
+    {
+        [Key]
+        public int codigo_cuadrilla { get; set; }
+        public string nombre_cuadrilla { get; set; }
+        public string localizacion { get; set; }
+
+    }
+}
